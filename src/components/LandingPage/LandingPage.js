f458748698eec8167320db0d6f8fe99e5dfca19b
sara <saraa379@yahoo.com>
2019-05-18 21:55:23 +0200

    typing

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -6,20 +6,58 @@ import {actionClickTab} from '../../actions/menuActions.js';
 import {LANDINGP_MENU, HOME_PATH} from '../constants.js';
 import {actionClickLang} from '../../actions/languageAction.js';
 
+//Global variables for typing function
+const texts = ['I', 'am', 'a', 'web', 'developer.', 'Welcome', 'to', 'my', 'web', 'portfolia', 'page.'];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+let sentence = '';
 
 class LandingPage extends Component {
+	constructor(props) {
+			super(props);
+			this.state = {content: ""
+									 };
+	}//end of constructor
+	//functions that update states
+	updateContent = (content) => {
+		this.setState({content});
+	}
+
 	componentDidMount(){
 		this.props.actionClickTab(LANDINGP_MENU);
+		this.timerID = setInterval(
+      () => this.type(), 400
+    );
 	}
 
 	//switches current language
-	langClick = (language) => {
-		this.props.actionClickLang(language);
+	type = () => {
+			if (count === texts.length) {
+				count = 0;
+			}
+
+			currentText = texts[count];
+			letter = currentText.slice(0, ++index);
+			//let newSentence = sentence + ' ' + letter;
+			//sentence = newSentence;
+			//this.updateContent(sentence);
+
+			if (letter.length === currentText.length) {
+				count++;
+				index = 0;
+			}
+			//setTimeout(type, 400);
 	}
+
+	componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
 
 	render() {
 		const {currentLanguage} = this.props.currentLanguage;
-		//console.log("Current language un home: " + currentLanguage);
+		const { content } = this.state;
 
 		return (
 			<div className="innerWrap LandingPage">
@@ -28,7 +66,7 @@ class LandingPage extends Component {
 
 							<div className="text">
 									<h2>Hi, my name is Sara.</h2>
-									<p>I am a web developer. Welcome to my web portfolia page!</p>
+									<p className="typer">{content}</p>
 
 									<div className="Language">
 										  <Link to={HOME_PATH}>
